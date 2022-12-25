6279a524c3909b2a709ad649d95810ad98bac600
Ali Zargar Shabestari <alizx.1370@gmail.com>
2022-12-25 17:25:43 -0500

    updating key generator

--- scripts/key-generator.py
+++ scripts/key-generator.py
@@ -1,7 +1,7 @@
 import hashlib
 import os
 
-def unwind(stm):
+def unwind(stm) -> bytes:
     _stm = hashlib.sha1(stm)
     return _stm.digest()
 
@@ -9,16 +9,14 @@ class PublisherState:
   def __init__(self, mw):
     self.i = 0
     self.stms = [None] * mw
-    self.stms[mw-1] = os.urandom(20) # 160 bit
+    self.stms[-1] = os.urandom(20) # 160 bit
 
-    for i in reversed(list(range(0,mw-1))):
+    for i in reversed(list(range(0, mw-1))):
         self.stms[i] = unwind(self.stms[i+1])
-    
 
 def wind(stp: PublisherState):
-    i = stp.i
     stp.i += 1
-    return (stp,stp.stms[i])
+    return (stp,stp.stms[stp.i])
 
 def keyder(stm):
     sha1 = hashlib.sha1(bytes([0]*8) + stm)
@@ -26,14 +24,15 @@ def keyder(stm):
 
 stp = PublisherState(5)
 
+
 last_stm = None
-for i in range(2):
+for i in range(4):
     (stp_next, stm) = wind(stp)
     last_stm = stm
     stp = stp_next
     key = keyder(stm)
     print(key)
 
-assert unwind(last_stm) == stp.stms[1]
-assert stp.i == 1
-
+assert unwind(last_stm) == stp.stms[3]
+assert unwind(unwind(last_stm)) == stp.stms[2]
+assert unwind(unwind(unwind(last_stm))) == stp.stms[1]
