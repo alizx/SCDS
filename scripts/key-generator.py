be6c3b6b1500595650550d4454de871d2c3e51a1
Ali Zargar Shabestari <alizx.1370@gmail.com>
2022-12-25 17:41:56 -0500

    update key generator to run tests only when the file is executed in isolation

--- scripts/key-generator.py
+++ scripts/key-generator.py
@@ -24,17 +24,17 @@ def keyder(stm):
     sha1 = hashlib.sha1(bytes([0]*8) + stm)
     return sha1.digest()
 
-stp = PublisherState(5)
+if __name__ == "__main__":
+    stp = PublisherState(5)
+    last_stm = None
+    
+    for i in range(4):
+        (stp_next, stm) = wind(stp)
+        last_stm = stm
+        stp = stp_next
+        key = keyder(stm)
+        print(key)
 
-
-last_stm = None
-for i in range(4):
-    (stp_next, stm) = wind(stp)
-    last_stm = stm
-    stp = stp_next
-    key = keyder(stm)
-    print(key)
-
-assert unwind(last_stm) == stp.stms[3]
-assert unwind(unwind(last_stm)) == stp.stms[2]
-assert unwind(unwind(unwind(last_stm))) == stp.stms[1]
+    assert unwind(last_stm) == stp.stms[3]
+    assert unwind(unwind(last_stm)) == stp.stms[2]
+    assert unwind(unwind(unwind(last_stm))) == stp.stms[1]
